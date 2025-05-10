4c09dd250592616a0e22dbad30ea16f52648bf87
Bufib <bufib.bufib@gmail.com>
2025-05-10 19:16:40 +0200

    tabview

--- app/(tabs)/knowledge/index.tsx
+++ app/(tabs)/knowledge/index.tsx
@@ -1,15 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import TopNavigation from "@/components/TopNavigation";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const index = () => {
-  return (
-      <TopNavigation />
-      
-  );
+  return <TopNavigation />;
 };
 
 export default index;
-
-
--- components/TopNavigation.tsx
+++ components/TopNavigation.tsx
@@ -1,74 +1,10 @@
-// import * as React from "react";
-// import {
-//   View,
-//   useWindowDimensions,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-// } from "react-native";
-// import { TabView, SceneMap } from "react-native-tab-view";
-// import prayers from "@/app/(tabs)/knowledge/(routes)/prayers";
-// import questions from "@/app/(tabs)/knowledge/(routes)/questions";
-// import i18n from "@/utils/i18n";
-// import { SafeAreaView } from "react-native-safe-area-context";
-// import AntDesign from "@expo/vector-icons/AntDesign";
-// const renderScene = SceneMap({
-//   questionsScreen: questions,
-//   prayerScreen: prayers,
-// });
-
-// const routes = [
-//   { key: "questionsScreen", title: i18n.t("questionScreenTitle") },
-//   { key: "prayerScreen", title: i18n.t("prayerScreenTitle") },
-// ];
-
-// export default function TopNavigation() {
-//   const layout = useWindowDimensions();
-//   const [index, setIndex] = React.useState(0);
-//   const colorScheme = useColorScheme() || "light";
-//   return (
-//     <SafeAreaView style={styles.container} edges={["top"]}>
-//       <View style={styles.headerIconContainer}>
-//         <AntDesign
-//           name="search1"
-//           size={24}
-//           color={colorScheme === "dark" ? "#fff" : "#000"}
-//           style={{ alignSelf: "flex-end", marginRight: 20 }}
-//         />
-//       </View>
-
-//       <TabView
-//         navigationState={{ index, routes }}
-//         renderScene={renderScene}
-//         onIndexChange={setIndex}
-//         initialLayout={{ width: layout.width }}
-//       />
-//     </SafeAreaView>
-//   );
-// }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   headerIconContainer: {
-//     marginBottom: 30,
-//   },
-// });
-
 import * as React from "react";
-import {
-  View,
-  useWindowDimensions,
-  StyleSheet,
-  Text,
-  useColorScheme,
-} from "react-native";
-import { TabView, SceneMap, TabBar } from "react-native-tab-view"; // Import TabBar
+import { useWindowDimensions, useColorScheme } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view"; 
 import prayers from "@/app/(tabs)/knowledge/(routes)/prayers";
 import questions from "@/app/(tabs)/knowledge/(routes)/questions";
 import i18n from "@/utils/i18n";
 import { SafeAreaView } from "react-native-safe-area-context";
-import AntDesign from "@expo/vector-icons/AntDesign";
 import { Colors } from "@/constants/Colors";
 
 const renderScene = SceneMap({
@@ -91,16 +27,7 @@ export default function TopNavigation() {
       <SafeAreaView
         style={[{ backgroundColor: Colors[colorScheme].contrast }]}
         edges={["top"]}
-      >
-        <View style={styles.headerIconContainer}>
-          <AntDesign
-            name="search1"
-            size={24}
-            color={colorScheme === "dark" ? "#fff" : "#000"}
-            style={{ alignSelf: "flex-end", marginRight: 20 }}
-          />
-        </View>
-      </SafeAreaView>
+      />
       <TabView
         navigationState={{ index, routes }}
         renderScene={renderScene}
@@ -121,12 +48,3 @@ export default function TopNavigation() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "green",
-    gap: 15,
-  },
-  headerIconContainer: {},
-});
