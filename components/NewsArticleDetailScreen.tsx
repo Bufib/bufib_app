777b1479be866527855897837f499cfc7d6687a2
Bufib <bufib.bufib@gmail.com>
2025-06-17 00:08:26 +0200

    news article

--- components/NewsArticleDetailScreen.tsx
+++ components/NewsArticleDetailScreen.tsx
@@ -1,253 +1,3 @@
-// import { ThemedText } from "@/components/ThemedText";
-// import { Colors } from "@/constants/Colors";
-// import { NewsArticlesType } from "@/constants/Types";
-// import { useNewsArticles } from "@/hooks/useNewsArticles";
-// import { useFontSizeStore } from "@/stores/fontSizeStore";
-// import { formattedDate } from "@/utils/formate";
-// import AntDesign from "@expo/vector-icons/AntDesign";
-// import Ionicons from "@expo/vector-icons/Ionicons";
-// import { Stack } from "expo-router";
-// import React, { useCallback, useEffect, useState } from "react";
-// import { useTranslation } from "react-i18next";
-// import { ScrollView, StyleSheet, useColorScheme, View } from "react-native";
-// import Markdown from "react-native-markdown-display";
-// import { LoadingIndicator } from "./LoadingIndicator";
-
-// import { useLanguage } from "@/contexts/LanguageContext";
-// import { useRefreshFavorites } from "@/stores/refreshFavoriteStore";
-// import { } from "@/utils/bufibDatabase";
-// import {
-//   isNewsArticleFavorited,
-//   toggleNewsArticleFavorite,
-// } from "@/utils/favorites";
-// import FontSizePickerModal from "./FontSizePickerModal";
-// import { ThemedView } from "./ThemedView";
-// export default function NewsArticleDetailScreen({
-//   articleId,
-// }: {
-//   articleId: number;
-// }) {
-//   const { fontSize, lineHeight } = useFontSizeStore();
-//   const colorScheme = useColorScheme() ?? "light";
-//   const { t } = useTranslation();
-//   const [article, setArticle] = useState<NewsArticlesType | null>(null);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [error, setError] = useState<string | null>(null);
-//   const [showFontSizePickerModal, setShowFontSizePickerModal] = useState(false);
-//   const [isFavorite, setIsFavorite] = useState(false);
-//   const { triggerRefreshFavorites } = useRefreshFavorites();
-//   const { language, isArabic } = useLanguage();
-//   const { fetchNewsArticleById } = useNewsArticles(language);
-
-//   useEffect(() => {
-//     if (!articleId) {
-//       setError(t("errorLoadingArticle"));
-//       setIsLoading(false);
-//       return;
-//     }
-
-//     const loadArticle = async () => {
-//       setIsLoading(true);
-//       setError(null);
-//       try {
-//         const fetchedArticle = await fetchNewsArticleById(articleId);
-//         if (fetchedArticle) {
-//           setArticle(fetchedArticle);
-//         } else {
-//           setError(t("errorLoadingArticle"));
-//         }
-//       } catch (error: any) {
-//         console.error("Error loading news article:", error);
-//         setError(error.message || t("errorLoadingArticle"));
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-
-//     loadArticle();
-//   }, [articleId]);
-
-//   // load favorite state
-//   useEffect(() => {
-//     (async () => {
-//       try {
-//         setIsFavorite(await isNewsArticleFavorited(articleId));
-//       } catch {
-//         console.log("error");
-//       }
-//     })();
-//   }, [articleId]);
-
-//   // ★ toggle handler updates local state
-//   const onPressToggle = useCallback(async () => {
-//     if (!articleId) return;
-
-//     try {
-//       const newFavStatus = await toggleNewsArticleFavorite(articleId);
-//       setIsFavorite(newFavStatus);
-//       triggerRefreshFavorites();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }, [articleId, triggerRefreshFavorites]);
-
-//   if (isLoading) {
-//     return (
-//       <ThemedView style={styles.centered}>
-//         <LoadingIndicator size={"large"} />
-//       </ThemedView>
-//     );
-//   }
-
-//   if (error || !article) {
-//     return (
-//       <ThemedView style={styles.centered}>
-//         <Stack.Screen options={{ title: "Error" }} />
-//         <ThemedText
-//           type="subtitle"
-//           style={[styles.errorText, { color: Colors[colorScheme].error }]}
-//         >
-//           {t("errorLoadingArticle")}
-//         </ThemedText>
-//       </ThemedView>
-//     );
-//   }
-//   return (
-//     <View style={styles.container}>
-//       <Stack.Screen
-//         options={{
-//           title: article.title,
-//         }}
-//       />
-//       <ScrollView
-//         style={[
-//           styles.scrollViewStyles,
-//           {
-//             backgroundColor: Colors[colorScheme].background,
-//           },
-//         ]}
-//         contentContainerStyle={styles.scrollViewContent}
-//         showsVerticalScrollIndicator={true}
-//       >
-//         <View
-//           style={[
-//             styles.titleContainer,
-//             {
-//               backgroundColor: Colors[colorScheme].contrast,
-//               borderColor: Colors[colorScheme].border,
-//             },
-//           ]}
-//         >
-//           <View
-//             style={{ flexDirection: "row", gap: 10, alignSelf: "flex-end" }}
-//           >
-//             <Ionicons
-//               name="text"
-//               size={30}
-//               color={Colors[colorScheme].defaultIcon}
-//               onPress={() => setShowFontSizePickerModal(true)}
-//             />
-//             {isFavorite ? (
-//               <AntDesign
-//                 name="star"
-//                 size={31}
-//                 color={Colors.universal.favorite}
-//                 onPress={() => onPressToggle()}
-//               />
-//             ) : (
-//               <AntDesign
-//                 name="staro"
-//                 size={31}
-//                 color={Colors[colorScheme].defaultIcon}
-//                 onPress={() => onPressToggle()}
-//               />
-//             )}
-//           </View>
-//           <ThemedText type="title" style={styles.titleText}>
-//             {article.title}
-//           </ThemedText>
-//           <View style={styles.titleIconContainer}>
-//             <View style={styles.titleIconText}>
-//               <Ionicons
-//                 name="calendar-number-sharp"
-//                 size={24}
-//                 color={Colors.universal.third}
-//               />
-//               <ThemedText>{formattedDate(article.created_at)}</ThemedText>
-//             </View>
-//             <View style={styles.titleIconText}>
-//               <AntDesign
-//                 name="clockcircleo"
-//                 size={24}
-//                 color={Colors.universal.third}
-//               />
-//               <ThemedText>{article.read_time} min</ThemedText>
-//             </View>
-//           </View>
-//         </View>
-//         <View style={styles.contentContainer}>
-//           <Markdown
-//             style={{
-//               body: {
-//                 color: Colors[colorScheme].text,
-//                 fontSize: fontSize,
-//                 lineHeight: lineHeight,
-//               },
-//             }}
-//           >
-//             {article.content}
-//           </Markdown>
-//         </View>
-//       </ScrollView>
-//       <FontSizePickerModal
-//         visible={showFontSizePickerModal}
-//         onClose={() => setShowFontSizePickerModal(false)}
-//       />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   scrollViewStyles: {},
-//   scrollViewContent: {},
-//   titleContainer: {
-//     flexDirection: "column",
-//     gap: 10,
-//     padding: 20,
-//     paddingTop: 20,
-//     borderBottomWidth: 1,
-//   },
-//   titleIconContainer: {
-//     flexDirection: "row",
-//     justifyContent: "space-evenly",
-//     marginTop: 20,
-//   },
-//   titleIconText: {
-//     flexDirection: "row",
-//     gap: 10,
-//   },
-
-//   titleText: {
-//     textAlign: "center",
-//   },
-//   contentContainer: {
-//     flex: 1,
-//     padding: 20,
-//   },
-//   centered: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     padding: 20,
-//   },
-//   errorText: {
-//     textAlign: "center",
-//   },
-// });
-
 import { Colors } from "@/constants/Colors";
 import { NewsArticlesType } from "@/constants/Types";
 import { useLanguage } from "@/contexts/LanguageContext";
@@ -262,13 +12,14 @@ import { formattedDate } from "@/utils/formate";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Image } from "expo-image";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import {
   Pressable,
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from "react-native";
@@ -297,6 +48,14 @@ export default function NewsArticleDetailScreen({
   const { triggerRefreshFavorites } = useRefreshFavorites();
   const { language, isArabic } = useLanguage();
   const { fetchNewsArticleById } = useNewsArticles(language || "de");
+
+  const scrollViewRef = useRef<ScrollView>(null);
+  const handleScroll = (event: any) => {
+    setScrollY(event.nativeEvent.contentOffset.y);
+  };
+  const scrollToTop = () => {
+    scrollViewRef.current?.scrollTo({ y: 0, animated: true });
+  };
 
   useEffect(() => {
     if (!articleId) {
@@ -347,10 +106,6 @@ export default function NewsArticleDetailScreen({
       console.log(error);
     }
   }, [articleId, triggerRefreshFavorites]);
-
-  const handleScroll = (event: any) => {
-    setScrollY(event.nativeEvent.contentOffset.y);
-  };
 
   if (isLoading) {
     return (
@@ -422,6 +177,7 @@ export default function NewsArticleDetailScreen({
         onScroll={handleScroll}
         scrollEventThrottle={16}
         showsVerticalScrollIndicator={false}
+        ref={scrollViewRef}
       >
         <View style={styles.heroSection}>
           <View style={[styles.header]}>
@@ -434,7 +190,7 @@ export default function NewsArticleDetailScreen({
                 },
               ]}
             >
-              {t("news").toUpperCase()}
+              {t("newsArticleScreenTitle").toUpperCase()}
             </Text>
           </View>
 
@@ -644,7 +400,10 @@ export default function NewsArticleDetailScreen({
             <View
               style={[
                 styles.footerContainer,
-                { borderColor: Colors[colorScheme].border },
+                {
+                  borderColor: Colors[colorScheme].border,
+                  alignItems: isArabic() ? "flex-end" : "flex-start",
+                },
               ]}
             >
               <ThemedText
@@ -714,6 +473,11 @@ export default function NewsArticleDetailScreen({
         visible={showFontSizePickerModal}
         onClose={() => setShowFontSizePickerModal(false)}
       />
+      {scrollY > 200 && (
+        <TouchableOpacity style={styles.arrowUp} onPress={scrollToTop}>
+          <AntDesign name="arrowup" size={30} color={Colors.universal.third} />
+        </TouchableOpacity>
+      )}
     </SafeAreaView>
   );
 }
@@ -735,7 +499,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginBottom: 15,
+    marginBottom: 20,
   },
   headerText: {
     color: "white",
@@ -866,5 +630,14 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     paddingBottom: 40,
     paddingHorizontal: 24,
+  },
+  arrowUp: {
+    position: "absolute",
+    bottom: "60%",
+    right: "3%",
+    borderWidth: 2.5,
+    borderRadius: 99,
+    padding: 5,
+    borderColor: Colors.universal.third,
   },
 });
