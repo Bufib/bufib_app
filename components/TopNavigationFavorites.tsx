f2874212e5bff3150d8afe19d37d1ff77dbf41b4
Bufib <bufib.bufib@gmail.com>
2025-09-08 04:03:26 +0200

    fix

--- components/TopNavigationFavorites.tsx
+++ components/TopNavigationFavorites.tsx
@@ -16,11 +16,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "@/constants/Colors";
 import { useLanguage } from "@/contexts/LanguageContext";
 import { useTranslation } from "react-i18next";
+import { Image } from "expo-image";
+import favoritePodcasts from "@/app/(tabs)/favorites/favoritePodcasts";
+import favoritePrayers from "@/app/(tabs)/favorites/favoritePrayers";
+import favoriteQuestions from "@/app/(tabs)/favorites/favoriteQuestions";
 const renderScene = SceneMap({
   favoriteNewsArticles: FavoriteNewsArticles,
   favoritePrayers: FavoritePrayers,
   favoriteQuestions: FavoriteQuestions,
-  favoritesPodcasts: FavoritePodcasts,
+  favoritePodcasts: FavoritePodcasts,
 });
 
 export default function TopNavigationFavorites() {
@@ -32,10 +36,31 @@ export default function TopNavigationFavorites() {
 
   const routes = React.useMemo(
     () => [
-      { key: "favoriteNewsArticles", title: t("newsArticleScreenTitle") },
-      { key: "favoritePrayers", title: t("prayerScreenTitle") },
-      { key: "favoriteQuestions", title: t("questionScreenTitle") },
-      { key: "favoritesPodcasts", title: t("podcastScreenTitle") },
+      {
+        key: "favoriteNewsArticles",
+        // title: t("newsArticleScreenTitle"),
+        title: "",
+        icon: require("@/assets/images/newsArticleHeaderLogo.png"),
+      },
+         {
+        key: "favoritePodcasts",
+        // title: t("podcastScreenTitle"),
+        title: "",
+        icon: require("@/assets/images/podcastHeaderLogo2.png"),
+      },
+      {
+        key: "favoritePrayers",
+        // title: t("prayerScreenTitle"),
+        title: "",
+        icon: require("@/assets/images/prayersHeaderLogo.png"),
+      },
+      {
+        key: "favoriteQuestions",
+        // title: t("questionScreenTitle"),
+        title: "",
+        icon: require("@/assets/images/qAndAHeaderLogo.png"),
+      },
+   
     ],
     [t]
   );
@@ -69,6 +94,60 @@ export default function TopNavigationFavorites() {
           renderScene={renderScene}
           onIndexChange={setIndex}
           initialLayout={{ width: layout.width }}
+          options={{
+            favoriteNewsArticles: {
+              icon: ({ route, focused, color }) => (
+                <Image
+                  source={route.icon}
+                  contentFit="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    opacity: focused ? 1 : 0.6,
+                  }}
+                />
+              ),
+            },
+            favoritePodcasts: {
+              icon: ({ route, focused, color }) => (
+                <Image
+                  source={route.icon}
+                  contentFit="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    opacity: focused ? 1 : 0.6,
+                  }}
+                />
+              ),
+            },
+            favoriteQuestions: {
+              icon: ({ route, focused, color }) => (
+                <Image
+                  source={route.icon}
+                  contentFit="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    opacity: focused ? 1 : 0.6,
+                  }}
+                />
+              ),
+            },
+            favoritePrayers: {
+              icon: ({ route, focused, color }) => (
+                <Image
+                  source={route.icon}
+                  contentFit="contain"
+                  style={{
+                    width: 35,
+                    height: 35,
+                    opacity: focused ? 1 : 0.6,
+                  }}
+                />
+              ),
+            },
+          }}
           renderTabBar={(props) => (
             <TabBar
               {...props}
